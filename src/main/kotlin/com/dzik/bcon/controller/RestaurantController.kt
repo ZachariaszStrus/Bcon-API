8fd4d66aa6dcf210ec3afc945ad099789522a021
zacstrus <zachariasz.strus@gmail.com>
2017-08-01 21:45:48 +0200

    null

--- src/main/kotlin/com/dzik/bcon/controller/RestaurantController.kt
+++ src/main/kotlin/com/dzik/bcon/controller/RestaurantController.kt
@@ -2,18 +2,21 @@ package com.dzik.bcon.controller
 
 import com.dzik.bcon.dao.RestaurantDao
 import com.dzik.bcon.restaurant.Restaurant
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController("/restaurants")
 class RestaurantController(val restaurantDao: RestaurantDao) {
-    private val log = LoggerFactory.getLogger(RestaurantController::class.java)
 
     @GetMapping
     fun getAll(): MutableList<Restaurant> {
-        log.info("Restaurant getAll")
         return restaurantDao.findAll()
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@RequestParam id: Int): Restaurant? {
+        return restaurantDao.find(id)
     }
 }
--- src/main/kotlin/com/dzik/bcon/dao/impl/RestaurantDaoImpl.kt
+++ src/main/kotlin/com/dzik/bcon/dao/impl/RestaurantDaoImpl.kt
@@ -4,26 +4,35 @@ import com.dzik.bcon.dao.RestaurantDao
 import com.dzik.bcon.restaurant.Restaurant
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
+import java.sql.ResultSet
+
+
+
+
 
 @Repository
 class RestaurantDaoImpl(val jdbcTemplate: JdbcTemplate) : RestaurantDao {
+    val restaurantMapper = { rs: ResultSet, rn: Int ->
+        Restaurant(rs.getInt("id"),rs.getString("name"),rs.getInt("menu_id"))
+    }
+
     override fun create(entity: Restaurant): Restaurant? {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun find(key: Int): Restaurant? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return jdbcTemplate.queryForObject(
+                "SELECT * FROM restaurant WHERE id = ? LIMIT 1",
+                arrayOf<Any>(key),
+                restaurantMapper
+        )
     }
 
     override fun findAll(): MutableList<Restaurant> {
         return jdbcTemplate.query(
-                "SELECT * FROM restaurant") { r, _ ->
-                    Restaurant(
-                            r.getInt("id"),
-                            r.getString("name"),
-                            r.getInt("menu_id")
-                    )
-                }
+                "SELECT * FROM restaurant",
+                restaurantMapper
+        )
     }
 
     override fun update(entity: Restaurant): Restaurant? {
