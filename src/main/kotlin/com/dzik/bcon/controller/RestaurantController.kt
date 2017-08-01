2ed2dbfa1db0a3df5ca8f5b9b63b31a921266287
zacstrus <zachariasz.strus@gmail.com>
2017-08-01 10:07:46 +0200

    null

--- src/main/kotlin/com/dzik/bcon/controller/RestaurantController.kt
+++ src/main/kotlin/com/dzik/bcon/controller/RestaurantController.kt
@@ -1,13 +1,29 @@
 package com.dzik.bcon.controller
 
+import com.dzik.bcon.model.MenuItem
+import org.slf4j.LoggerFactory
+import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class RestaurantController {
+class RestaurantController(val jdbcTemplate: JdbcTemplate) {
+    private val log = LoggerFactory.getLogger(RestaurantController::class.java)
+
+
     @RequestMapping("/env")
-    fun greeting(): MutableMap<String, String>? {
-        return System.getenv()
+    fun greeting(): MutableList<MenuItem>? {
+        val menuItem = jdbcTemplate.query(
+                "SELECT id, name, price, menu_id FROM customers WHERE first_name = ?")
+        { rs, rowNum ->
+            MenuItem(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getFloat("price"),
+                    rs.getInt("menu_id")
+            )
+        }
+        return menuItem
     }
 }
---
+++ src/main/kotlin/com/dzik/bcon/model/MenuItem.kt
@@ -0,0 +1,9 @@
+package com.dzik.bcon.model
+
+
+data class MenuItem(
+        val id: Int,
+        val name: String,
+        val price: Float,
+        val menuId: Int
+)
