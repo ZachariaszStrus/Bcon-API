773f27c6f9f05b1a55079e9ece8d58bf0501a3b1
zacstrus <zachariasz.strus@gmail.com>
2017-08-01 21:54:18 +0200

    null

--- src/main/kotlin/com/dzik/bcon/controller/RestaurantController.kt
+++ src/main/kotlin/com/dzik/bcon/controller/RestaurantController.kt
@@ -3,11 +3,13 @@ package com.dzik.bcon.controller
 import com.dzik.bcon.dao.RestaurantDao
 import com.dzik.bcon.restaurant.Restaurant
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
-@RestController("/restaurants")
+@RestController
+@RequestMapping("/restaurants")
 class RestaurantController(val restaurantDao: RestaurantDao) {
 
     @GetMapping
