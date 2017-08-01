47217c4b303f9886d2196291899552f25b930355
zacstrus <zachariasz.strus@gmail.com>
2017-08-01 09:37:15 +0200

    null

--- src/main/kotlin/com/dzik/bcon/BconApplication.kt
+++ src/main/kotlin/com/dzik/bcon/BconApplication.kt
@@ -1,10 +1,20 @@
 package com.dzik.bcon
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.jdbc.core.JdbcTemplate
+
+
 
 @SpringBootApplication
-class BconApplication
+class BconApplication(val jdbcTemplate: JdbcTemplate) : CommandLineRunner{
+
+    override fun run(vararg p0: String?) {
+        System.out.println(jdbcTemplate)
+    }
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(BconApplication::class.java, *args)
