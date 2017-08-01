28ce328f61d8683cb9a1edd816d91f06a4d71a50
zacstrus <zachariasz.strus@gmail.com>
2017-08-01 09:26:44 +0200

    null

--- src/main/kotlin/com/dzik/bcon/controller/RestaurantController.kt
+++ src/main/kotlin/com/dzik/bcon/controller/RestaurantController.kt
@@ -1,15 +1,13 @@
 package com.dzik.bcon.controller
 
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
-
-
+import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 class RestaurantController {
-    @RequestMapping("/")
-    fun greeting(): String {
-        return "Hello Bcon Spring"
+    @RequestMapping("/env")
+    fun greeting(): MutableMap<String, String>? {
+        return System.getenv()
     }
 }
