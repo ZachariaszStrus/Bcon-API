4fe7216334fb5b33cd2ed60205cfb0f0c2b1fae8
zacstrus <zachariasz.strus@gmail.com>
2017-10-12 20:29:00 +0200

    socket test

--- src/main/kotlin/com/dzik/bcon/controller/UserController.kt
+++ src/main/kotlin/com/dzik/bcon/controller/UserController.kt
@@ -1,20 +1,35 @@
 package com.dzik.bcon.controller
 
 import com.dzik.bcon.dao.UserRepository
+import com.dzik.bcon.model.Order
 import com.dzik.bcon.model.User
+import org.springframework.http.ResponseEntity
+import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/test")
 class UserController(
-        val userRepository: UserRepository
+        val userRepository: UserRepository,
+        val simpMessagingTemplate: SimpMessagingTemplate
 ) {
 
     @GetMapping
     fun getUsers(): MutableList<User>? {
         return userRepository.findAll()
+    }
+
+    @GetMapping("/{id}")
+    fun sendMessage(@PathVariable id: Int): ResponseEntity<Boolean> {
+        this.simpMessagingTemplate.convertAndSend(
+                "/topic/orders/" + id,
+                Order()
+        )
+
+        return ResponseEntity.ok(true)
     }
 }
