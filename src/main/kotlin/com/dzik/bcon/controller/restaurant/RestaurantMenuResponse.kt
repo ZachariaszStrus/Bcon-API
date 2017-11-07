1c6a4f8a026cf0c20e879af2807788982877a0a8
zaki <zachariasz.strus@gmail.com>
2017-11-07 09:30:33 +0100

    beacons & tables

--- src/main/kotlin/com/dzik/bcon/controller/restaurant/RestaurantMenuResponse.kt
+++ src/main/kotlin/com/dzik/bcon/controller/restaurant/RestaurantMenuResponse.kt
@@ -5,10 +5,12 @@ import com.dzik.bcon.model.Restaurant
 
 
 data class RestaurantMenuResponse (
+        val id: Int,
         val name: String,
         val menu: Set<MenuItem>
 ) {
     constructor(restaurant: Restaurant) : this (
+            restaurant.id,
             restaurant.name,
             restaurant.menuItems
     )
